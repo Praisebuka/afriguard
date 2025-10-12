bca1fa67357ff672e8ec8f872a972c868a2b5fef
Praisebuka <praise.njoga@gmail.com>
2025-10-12 20:03:10 +0100

    methd and calls - correct!

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,16 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\PentestingController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('login', [LoginController::class, 'login'])->name('login');
 
+Route::get('/v1', function () {
+    return response()->json([ 'status' => 'success', 'message' => 'AfriGuard Version 1.0', 'data' => [ 'v1' => url('api/v1/'), 's' => $_SERVER['REMOTE_ADDR'], ]]);
+});
 
+Route::prefix('/v1')->group(function () {
+    Route::post('login', [LoginController::class, 'login'])->name('login');
 
-Route::group(['prefix' => 'v1', 'as' => 'api.', 'namespace' => 'api\v1', 'middleware' => ['auth:api']], function () {
-    Route::post('/nmap-scan', [PentestingController::class, 'nmapScan']);
+    Route::group(['middleware' => ['auth:api']], function () {
+        Route::post('/nmap-scan', [PentestingController::class, 'nmapScan']);
+    });
+
 });
