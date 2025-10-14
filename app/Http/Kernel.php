e06ac75ea14e781ed78a3add911a57b6f13ef620
Praisebuka <praise.njoga@gmail.com>
2025-10-14 21:45:39 +0100

    maybe this will help?

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -31,6 +31,7 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\AuthGates::class,
             \App\Http\Middleware\SetLocale::class,
+            \Fruitcake\Cors\HandleCors::class,
         ],
     ];
 
