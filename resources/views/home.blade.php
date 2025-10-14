8598ab63f657d0013226642b581db35ef3d456a4
Praisebuka <praise.njoga@gmail.com>
2025-10-14 12:55:40 +0100

    ..

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,36 +18,6 @@
 
   @include('sections.faq')
 
-  <script>
-@if (session('success'))
-    Swal.fire({
-        title: 'Success!',
-        text: "{{ session('success') }}",
-        icon: 'success',
-        confirmButtonColor: '#3085d6',
-        confirmButtonText: 'OK'
-    });
-@endif
-
-@if (session('error'))
-    Swal.fire({
-        title: 'Error!',
-        text: "{{ session('error') }}",
-        icon: 'error',
-        confirmButtonColor: '#d33',
-        confirmButtonText: 'Try Again'
-    });
-@endif
-
-@if ($errors->any())
-    Swal.fire({
-        title: 'Validation Error!',
-        html: `{!! implode('<br>', $errors->all()) !!}`,
-        icon: 'warning',
-        confirmButtonText: 'Fix it'
-    });
-@endif
-</script>
 
 </main>
 @endsection
