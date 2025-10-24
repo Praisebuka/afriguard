58ea36bceec827884895032fd2a6b783c9838c1d
Praisebuka <praise.njoga@gmail.com>
2025-10-24 12:26:16 +0100

    mailable

--- app/Mail/ScanCompletedMvp.php
+++ app/Mail/ScanCompletedMvp.php
@@ -2,6 +2,7 @@
 
 namespace App\Mail;
 
+use App\Models\NmapRequest;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
@@ -11,14 +12,16 @@ class ScanCompletedMvp extends Mailable
 {
     use Queueable, SerializesModels;
 
+    public $nmapRequest;
+
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(NmapRequest $nmapRequest)
     {
-        //
+        $this->nmapRequest = $nmapRequest;
     }
 
     /**
@@ -28,6 +31,7 @@ class ScanCompletedMvp extends Mailable
      */
     public function build()
     {
-        return $this->view('view.name');
+        return $this->subject('Scan Completed: ' . $this->nmapRequest->target)
+                    ->markdown('emails. ');
     }
 }
--- app/Mail/ScanStarted.php
+++ app/Mail/ScanStarted.php
@@ -2,6 +2,7 @@
 
 namespace App\Mail;
 
+use App\Models\NmapRequest;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
@@ -11,14 +12,16 @@ class ScanStarted extends Mailable
 {
     use Queueable, SerializesModels;
 
+    public $nmapRequest;
+
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(NmapRequest $nmapRequest)
     {
-        //
+        $this->nmapRequest = $nmapRequest;
     }
 
     /**
@@ -28,6 +31,7 @@ class ScanStarted extends Mailable
      */
     public function build()
     {
-        return $this->view('view.name');
+        return $this->subject('Your Scan Has Started: ' . $this->nmapRequest->target)
+                    ->markdown('emails.scans.scan_started');
     }
 }
